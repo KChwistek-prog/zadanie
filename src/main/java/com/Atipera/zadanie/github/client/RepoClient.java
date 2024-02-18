df05f32c9046211f552d5658ba5fd9f1bac22112
Krzysztof <k.chwistek@outlook.com>
2024-02-18 17:48:23 +0100

    refactor

--- src/main/java/com/Atipera/zadanie/github/client/RepoClient.java
+++ src/main/java/com/Atipera/zadanie/github/client/RepoClient.java
@@ -22,60 +22,84 @@ import java.util.List;
 @Component
 public class RepoClient {
 
+    private static final String GITHUB_API_URL = "https://api.github.com";
+    private static final String AUTHORIZATION_HEADER = "authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+    private static final String X_GITHUB_API_VERSION_HEADER = "X-GitHub-Api-Version";
+    private static final String API_VERSION = "2022-11-28";
+    private static final String ACCEPT_HEADER = "accept";
+    private static final String ACCEPT_VALUE = "Accept: application/json";
+    private static final String TYPE_HEADER = "type";
+    private static final String TYPE_VALUE = "owner";
+    private static final String USER_NOT_FOUND_STATUS = "404";
+    private static final String USER_NOT_FOUND_MESSAGE = "User does not exist";
+
     @Value("${user.authenticationToken}")
     private String token;
 
-    @Autowired
-    public RepoClient() {
-    }
+    private final ObjectMapper objectMapper;
+    private HttpClient client = HttpClient.newHttpClient();
 
-    ObjectMapper objectMapper = new ObjectMapper();
-    HttpClient client = HttpClient.newHttpClient();
+    @Autowired
+    public RepoClient(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     public List<Response> getList(String username) throws IOException, InterruptedException {
         List<Response> resultData = new ArrayList<>();
-        Response response;
         try {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://api.github.com/users/" + username + "/repos"))
-                    .header("authorization", "Bearer " + token)
-                    .header("X-GitHub-Api-Version", "2022-11-28")
-                    .header("accept", "Accept: application/json")
-                    .header("type", "owner")
-                    .build();
+            HttpRequest request = createRequestForUserRepos(username);
             HttpResponse<String> clientResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-            Repos[] data = objectMapper.readValue(clientResponse.body(), Repos[].class);
-            for (Repos repos : data) {
-                Branches[] branch = getBranchList(repos.owner().login(), repos.name());
-                response = Response.builder()
-                        .repositoryName(repos.name())
-                        .owner(repos.owner().login())
-                        .branch(branch[0].name())
-                        .sha(branch[0].commit().sha())
-                        .build();
+            Repos[] repos = objectMapper.readValue(clientResponse.body(), Repos[].class);
+            for (Repos repo : repos) {
+                Response response = createResponseForRepo(repo);
                 resultData.add(response);
             }
-
         } catch (MismatchedInputException e) {
-            response = Response.builder()
-                    .status("404")
-                    .message("User does not exist")
-                    .build();
+            Response response = createResponseForUserNotFound();
             resultData.add(response);
         }
         return resultData;
     }
 
-    private Branches[] getBranchList(String owner, String repo) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.github.com/repos/" + owner + "/" + repo + "/branches"))
-                .header("authorization", "Bearer " + token)
-                .header("accept", "Accept: application/json")
+    private HttpRequest createRequestForUserRepos(String username) {
+        return HttpRequest.newBuilder()
+                .uri(URI.create(GITHUB_API_URL + "/users/" + username + "/repos"))
+                .header(AUTHORIZATION_HEADER, BEARER_PREFIX + token)
+                .header(X_GITHUB_API_VERSION_HEADER, API_VERSION)
+                .header(ACCEPT_HEADER, ACCEPT_VALUE)
+                .header(TYPE_HEADER, TYPE_VALUE)
                 .build();
-        HttpResponse<String> response = client.send(request,
-                HttpResponse.BodyHandlers.ofString());
+    }
 
+    private Response createResponseForRepo(Repos repo) throws IOException, InterruptedException {
+        Branches[] branches = getBranchList(repo.owner().login(), repo.name());
+        return Response.builder()
+                .repositoryName(repo.name())
+                .owner(repo.owner().login())
+                .branch(branches[0].name())
+                .sha(branches[0].commit().sha())
+                .build();
+    }
+
+    private Response createResponseForUserNotFound() {
+        return Response.builder()
+                .status(USER_NOT_FOUND_STATUS)
+                .message(USER_NOT_FOUND_MESSAGE)
+                .build();
+    }
+
+    private Branches[] getBranchList(String owner, String repo) throws IOException, InterruptedException {
+        HttpRequest request = createRequestForRepoBranches(owner, repo);
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         return objectMapper.readValue(response.body(), Branches[].class);
     }
 
+    private HttpRequest createRequestForRepoBranches(String owner, String repo) {
+        return HttpRequest.newBuilder()
+                .uri(URI.create(GITHUB_API_URL + "/repos/" + owner + "/" + repo + "/branches"))
+                .header(AUTHORIZATION_HEADER, BEARER_PREFIX + token)
+                .header(ACCEPT_HEADER, ACCEPT_VALUE)
+                .build();
+    }
 }
